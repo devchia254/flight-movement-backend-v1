7ef64f036994939d91f014808eec3d02b6857bd5
alex <alex7chiaster@gmail.com>
2020-09-16 10:23:42 +0800

    added initial.config to create initial data whenever the server restarts

--- server.js
+++ server.js
@@ -17,29 +17,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./models");
+const initial = require("./config/initial.config");
 const Role = db.role;
+const User = db.user;
 
 // In development, you may need to drop existing tables and re-sync database. So you can use force: true as code below.
 // force: true actually rewrites the records after every server restart
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and Resync Db");
-  initial();
+  initial.administrator(Role, User);
 });
 // For production, just insert these rows manually and use sync() without parameters to avoid dropping data:
 // db.sequelize.sync();
-
-// 'initial' helps create 3 rows in the DB
-function initial() {
-  Role.create({
-    role_id: 1,
-    role_type: "user",
-  });
-
-  Role.create({
-    role_id: 2,
-    role_type: "admin",
-  });
-}
 
 // simple route
 app.get("/", (req, res) => {
